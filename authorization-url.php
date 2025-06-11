4261df43cb4fd1d3974b5e232212fbd502de0d32
sagar211554 <kambojsagar9231@gmail.com>
2025-06-11 11:15:51 +0530

    Add files via upload

---
+++ authorization-url.php
@@ -0,0 +1,11 @@
+https://airtable.com/oauth2/v1/authorize?client_id=67318d2e-2198-4df7-bd17-79203158be42&redirect_uri=https://www.google.com/&response_type=code&scope=data.records:read%20data.records:write&state=XQ.2ca7AjSTEk.vIGIJ6xSHzvZJw-Te8&code_challenge=pHiqMP2QyG3z7zz7wSuV5mhQjdUlcSyW3E_--WDG_fQ&code_challenge_method=S256
+
+
+
+
+https://airtable.com/oauth2/v1/authorize?client_id=67318d2e-2198-4df7-bd17-79203158be42&redirect_uri=https://mdev.topscripts.in/testmdev/index.php&response_type=code&scope=data.records:read&data.records:write&state=&code_challenge=&code_challenge_method=S256
+
+https://airtable.com/oauth2/v1/authorize?client_id=67318d2e-2198-4df7-bd17-79203158be42&redirect_uri=https://mdev.topscripts.in/testmdev/index.php&response_type=code&scope=data.records:read&data.records:write&state=2lKft6QR5iJ94_Tc&code_challenge=lM1UVrpQw1YShyb0HIecGed2gjIeJjzNk0P38t70w&code_challenge_method=S256
+
+
+https://airtable.com/oauth2/v1/authorize?client_id=67318d2e-2198-4df7-bd17-79203158be42&redirect_uri=https://www.google.com/&response_type=code&scope=data.records:read%20data.records:write&state=XQ.2ca7AjSTEk.vIGIJ6xSHzvZJw-Te8&code_challenge=pHiqMP2QyG3z7zz7wSuV5mhQjdUlcSyW3E_--WDG_fQ&code_challenge_method=S256
